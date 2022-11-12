b836db60d186e8f21b388fb430628cf0401827af
Christine <110146143+crzhang94@users.noreply.github.com>
2022-11-11 23:56:11 -0800

    Resizing the board now resets the board colors too

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// DOM Elements
-
 const grid = document.querySelector('.grid');
 const resize = document.getElementById('resize');
 
-// Create a 16 x 16 grid of square divs
+// Sets default size to 16x16 at start
+let size = 16;
+
 
 // Make a square div
 function makeDiv() {
@@ -20,20 +20,34 @@ function makeDiv() {
 // Make a grid
 function makeGrid() {
     // Go through rows
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < size; i++) {
         // Go through columns
-        for (j = 0; j < 16; j++) {
+        for (j = 0; j < size; j++) {
             makeDiv();
         } 
     } 
-    
 }
 
 makeGrid();
+const squares = document.querySelectorAll('.square');
 
-// Resize button sends a popup asking for user input for grid size
+// Prompt user for size input when hitting resize button and creates new grid
 resize.addEventListener('click', () => {
-    prompt("Please enter desired number of squares per grid edge: _ x _");
+    size = prompt("Please enter desired number of squares per grid edge:"); 
+    grid.style.gridTemplateColumns = "repeat(" + size + " ,1fr)";
+    grid.style.gridTemplateRows = "repeat(" + size + " ,1fr)";
+    makeGrid();
+    resetGrid();
 })
 
+// Clear the grid
+function resetGrid() {
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    })
+}
+
 // Set the limit for user input to a max of 100
+
+
+// Figure out how to set square borders without affecting grid size
